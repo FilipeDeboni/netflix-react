9c9e05c256a4c36ddce9cbc233ef55bad6c6a5c2
Filipe <deboni.filipe@gmail.com>
2021-01-15 21:14:40 -0300

    API requests done

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,5 +1,4 @@
-const API_KEY = '83bdfe773393f60d7078c852574bad82';
-const API_BASE = 'https://api.themoviedb.org/3';
+
 
 const basicFetch = async (endpoint) => {
     const req = await fetch(`${API_BASE}${endpoint}`);
